1cbbe0fccdd3ca6e16f19ae921ba65ded0988844
Duality-afk <71151464+Duality-afk@users.noreply.github.com>
2021-11-11 17:43:23 +0530

    Update settings.py

--- anonimo/settings.py
+++ anonimo/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = (os.path.join(BASE_DIR,"static/"))
+
 MEDIA_URL = '/images/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static/")
-#STATICFILES_DIRS = (os.path.join(BASE_DIR,"static/")),
-MEDIA_ROOT = os.path.join(BASE_DIR, "images/")
+MEDIA_ROOT = (os.path.join(BASE_DIR, "images/"))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
