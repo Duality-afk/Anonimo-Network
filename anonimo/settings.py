a3e3dabfaf320e4bfc8b15100034a380c2365e04
Duality-afk <71151464+Duality-afk@users.noreply.github.com>
2021-11-11 17:33:09 +0530

    Update settings.py

--- anonimo/settings.py
+++ anonimo/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static/")
